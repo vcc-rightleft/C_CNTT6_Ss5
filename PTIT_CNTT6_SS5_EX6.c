08f5dcb45727c275d34bb3caa2187fadecab8b4b
vcc-rightleft <trumfaifai00ngo@gmail.com>
2025-07-31 08:59:37 +0700

    Update PTIT_CNTT6_SS5_EX6.c

--- PTIT_CNTT6_SS5_EX6.c
+++ PTIT_CNTT6_SS5_EX6.c
@@ -25,7 +25,7 @@ float a,b,sum;
             printf("ket qua cua ban la : %.0f",sum);
             break;
         case '/':
-            if(a==0,b==0) {
+            if(b==0) {
                 printf("khong the chia cho 0");
             }else if (a<b){
                 printf("phep toan nay kho");
